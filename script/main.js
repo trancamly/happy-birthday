74f6f5efc1b164ce64b41d8cb662e88c023ecb04
trancamly <trancamlyuahn9212@gmail.com>
2024-08-07 02:26:25 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -131,6 +131,8 @@ const animationTimeline = () => {
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-6", 0.7, ideaTextTrans)
+    .to("idea-6", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-3", 0.7, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 1.2,
